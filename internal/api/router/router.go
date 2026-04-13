8096ae93e9413dfbf67d58a78551a7516418641c
steamban <arvind.nair@one2n.in>
2026-04-13 17:37:48 +0530

    chore: fixes indentation

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -11,16 +11,12 @@ func SetupRouter(studentHandler *handler.StudentHandler) *gin.Engine {
 
 	// API version 1
 	v1 := r.Group("/api/v1")
-	{
-		students := v1.Group("/students")
-		{
-			students.POST("", studentHandler.CreateStudent)
-			students.GET("", studentHandler.GetStudents)
-			students.GET("/:id", studentHandler.GetStudentByID)
-			students.PATCH("/:id", studentHandler.UpdateStudent)
-			students.DELETE("/:id", studentHandler.DeleteStudent)
-		}
-	}
+	students := v1.Group("/students")
+	students.POST("", studentHandler.CreateStudent)
+	students.GET("", studentHandler.GetStudents)
+	students.GET("/:id", studentHandler.GetStudentByID)
+	students.PATCH("/:id", studentHandler.UpdateStudent)
+	students.DELETE("/:id", studentHandler.DeleteStudent)
 
 	return r
 }
